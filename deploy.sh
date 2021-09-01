5b44b4d995faf5bcaa5b3d705ad0672d53e0f0df
Douglas Cardoso Rezende Morais <Douglas.CardosoRezendeMorais@windriver.com>
2021-09-01 17:43:58 -0300

    Release new version of the app

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 
 sed -i 's/http:\/\/localhost:5000/https:\/\/vagaspramim\.herokuapp\.com/' templates/index.html
 sed -i 's/\"enabled\": False/\"enabled\": True/' src/crawler/factory.py
-
+sed -i 's/DEBUG \= True\DEBUG \= False/' src/settings.py
 pip freeze > requirements.txt
 git status
 git add .
