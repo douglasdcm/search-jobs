37fcea3976cbf1bac0dbd6e08a73bcec46aeff3b
Douglas Cardoso Rezende Morais <Douglas.CardosoRezendeMorais@windriver.com>
2021-10-25 17:03:17 -0300

    Using ngram_range=(1, 2) to get terms related to each position

--- src/similarity/similarity.py
+++ src/similarity/similarity.py
@@ -6,7 +6,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 class Similarity:
 
     def __init__(self):
-        self.bow = CountVectorizer(binary=True)
+        self.bow = CountVectorizer(binary=True, ngram_range=(1, 2))
 
     def return_similarity_by_cossine(self, cv, positions):
         """
