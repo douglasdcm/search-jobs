3b73b33aa88675c152d5c69eb6dfd04c0baf7dcd
Douglas Cardoso Rezende Morais <Douglas.CardosoRezendeMorais@windriver.com>
2021-10-26 23:33:01 -0300

    fixing errors in log

--- debug.py
+++
@@ -1,6 +0,0 @@
-import logging
-from src.settings import LOGS_FILE
-logging.FileHandler(filename=LOGS_FILE, mode='w', encoding=None, delay=False)
-logging.basicConfig(
-    format='%(asctime)s %(levelname)-8s %(message)s',
-    filename=LOGS_FILE, level=logging.ERROR, datefmt='%Y-%m-%d %H:%M:%S')
