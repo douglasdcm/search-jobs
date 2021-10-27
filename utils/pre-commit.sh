9b13e715e9e737d0aa6cced38ce00e3d3e12b81a
Douglas Cardoso Rezende Morais <Douglas.CardosoRezendeMorais@windriver.com>
2021-10-26 23:39:14 -0300

    fixing error with log

--- utils/pre-commit.sh
+++ utils/pre-commit.sh
@@ -1,5 +1,6 @@
 
 echo "Changing source code to be deployed."
+echo "" > ./logs/crawler.log
 sed -i 's/\"enabled\": False/\"enabled\": True/' src/crawler/factory.py
 sed -i 's/DEBUG \= True/DEBUG \= False/' src/settings.py
 pip freeze > requirements.txt
