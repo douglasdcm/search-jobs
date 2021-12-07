8ad4cc44e7dbd05ca1fb67abafc3f5c86863ba04
Douglas Cardoso Rezende Morais <Douglas.CardosoRezendeMorais@windriver.com>
2021-12-07 01:31:16 -0300

    Adding service for web driver

--- app.py
+++ app.py
@@ -16,6 +16,9 @@ def service_db():
     dbf = DbFactory(DB_TYPE["p"])
     return dbf.get_db(DB_NAME)
 
+def service_driver():
+    return ChromeDriver()
+
 @app.route('/')
 def output():
     # serve index template
@@ -51,7 +54,7 @@ def update_():
     if os.getenv('HASH') == "":
         os.environ['HASH'] = "dev"
     if data["hash"] == os.getenv('HASH'):
-        update(service_db(), ChromeDriver(), Factory().get_crawlers())
+        update(service_db(), service_driver(), Factory().get_crawlers())
         return "OK\n"
     else:
         return "NO ACTION\n"
