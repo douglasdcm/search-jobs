99fda6f69d29a5693cb2212500986eeec2972ecd
Cardoso Rezende Morais, Douglas <Douglas.CardosoRezendeMorais@windriver.com>
2022-12-09 10:36:23 -0300

    Fixing heroku integration

--- utils/setup.sh
+++ utils/setup.sh
@@ -1,11 +1,12 @@
 # prepare the container and install the dependencies
-BASE_DIR="/"
+BASE_DIR="/opt/webapp"
 # install the dependencies
 apt-get update
 apt-get -y install libnss3-tools
 apt-get -y install cron
 apt update
 apt install -y ${BASE_DIR}/src/resources/google-chrome-stable_current_amd64.deb
+# pip install --upgrade pip
 pip install -r ${BASE_DIR}/requirements.txt
 # modify files
 # change the chrome driver to version 94
@@ -14,10 +15,10 @@ mv ${BASE_DIR}/src/resources/chromedriver94 /src/resources/chromedriver
 mkdir ${BASE_DIR}/logs
 echo "" > ${BASE_DIR}/logs/crawler.log
 # Configure crontab
-CRON_FILE="${BASE_DIR}/utils/update-cron"
+# CRON_FILE="${BASE_DIR}/utils/update-cron"
 # Give execution rights on the cron job
-chmod 0644 ${CRON_FILE}
+# chmod 0644 ${CRON_FILE}
 # Apply cron job
-crontab ${CRON_FILE}
+# crontab ${CRON_FILE}
 # Create the log file to be able to run tail
-touch /var/log/cron.log
+# touch /var/log/cron.log
