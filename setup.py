89827e304537891b4d232dc10ae7a5139305e8f4
Douglas Cardoso <douglas.dcm@gmail.com>
2023-04-19 22:04:21 -0300

    added tox and the log folder to container

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup (
+    name='vagaspramim',
+    version='1.0',
+    description='Python Distribution Utilities',
+    author='Douglas Cardoso',
+    author_email='email@email.com',
+    url='https://github.com/douglasdcm/crawler_of_positions',
+    packages=[
+        'src',
+    ],
+)
