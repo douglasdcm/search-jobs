0179814da9e5b703f1bd3528fa16bf1f77353359
Douglas Cardoso <douglas.dcm@gmail.com>
2023-05-16 00:34:40 -0300

    adding poc for assynchronous selenium

---
+++ poc_arsenic.py
@@ -0,0 +1,31 @@
+import asyncio
+import sys
+
+from arsenic import get_session, keys, browsers, services
+
+if sys.platform.startswith('win'):
+    GECKODRIVER = './geckodriver.exe'
+else:
+    GECKODRIVER = './geckodriver'
+
+
+async def hello_world():
+    # service = services.Geckodriver(binary=GECKODRIVER)
+    # browser = browsers.Firefox()
+    service = services.Chromedriver(binary="./src/resources/chromedriver")
+    browser = browsers.Chrome()
+    async with get_session(service, browser) as session:
+        await session.get('https://images.google.com/')
+        search_box = await session.wait_for_element(5, 'textarea[name=q]')
+        await search_box.send_keys('Cats')
+        await search_box.send_keys(keys.ENTER)
+        await asyncio.sleep(10)
+
+
+def main():
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(hello_world())
+
+
+if __name__ == '__main__':
+    main()
