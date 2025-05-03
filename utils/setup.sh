0ade9d85c2a0d53c6cf83929e3bb79a82cf586ea
Douglas Cardoso <29078346+douglasdcm@users.noreply.github.com>
2025-05-03 00:04:01 -0300

    Add python-psycopg2

--- utils/setup.sh
+++ utils/setup.sh
@@ -5,6 +5,7 @@ apt-get -y install libnss3-tools
 apt-get -y install cron
 apt update
 apt install -y ${BASE_DIR}/src/resources/google-chrome-stable_current_amd64.deb
+apt-get install build-dep python-psycopg2
 pip install -r ${BASE_DIR}/requirements.txt
 mkdir -p ${BASE_DIR}/logs
 echo "" > ${BASE_DIR}/logs/crawler.log
